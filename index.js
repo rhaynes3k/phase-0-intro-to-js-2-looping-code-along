f569c2b79f68fad901692d89919a113e93ab4334
rhaynes3k <rhaynes3k@gmail.com>
2022-05-16 17:19:16 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+function writeCards(arr, event) {
+  let msg = []
+  for(let m = 0; m < arr.length; m++){
+    msg.push( `Thank you, ${arr[m]}, for the wonderful ${event} gift!` )
+  }
+  return msg
+}
+
+function countDown(num) {
+  let m = 0
+  while(m <= num){
+    console.log(num - m)
+    m++
+  }
+}
